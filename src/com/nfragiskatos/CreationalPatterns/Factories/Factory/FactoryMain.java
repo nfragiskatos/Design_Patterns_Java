d6d5a54ce29c539cdd4377ea6d4d93007f9a701e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-22 13:16:23 -0700

    - Not really a GOF design pattern, but another way you can create objects by having an internal public factory class while the outer class has private constructor/fields, and the internal class uses static methods to create the outer class instances. - This makes it so the client has 1 and only 1 way of creating the object, and that's through the static factory class.

---
+++ src/com/nfragiskatos/CreationalPatterns/Factories/Factory/FactoryMain.java
@@ -0,0 +1,8 @@
+package com.nfragiskatos.CreationalPatterns.Factories.Factory;
+
+public class FactoryMain {
+    public static void main(String[] args) {
+        Point point = Point.Factory.newCartesianPoint(2, 3);
+
+    }
+}
