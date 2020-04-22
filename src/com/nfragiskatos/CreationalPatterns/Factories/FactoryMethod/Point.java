9604cfb12502d57d2e987c221aff4fab4e09e4d1
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-22 13:07:33 -0700

    - Basic factory method patter instead of overloading a bunch of constructors, and even skirting around when we can't overload constructors if the argument types are identical.

---
+++ src/com/nfragiskatos/CreationalPatterns/Factories/FactoryMethod/Point.java
@@ -0,0 +1,18 @@
+package com.nfragiskatos.CreationalPatterns.Factories.FactoryMethod;
+
+public class Point {
+    private double x,y;
+
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+    }
+}
