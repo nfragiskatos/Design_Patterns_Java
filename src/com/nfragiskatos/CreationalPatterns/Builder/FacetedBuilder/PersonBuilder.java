b46c0182e3b1024ceb5ac1150b707c4ee488b506
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-22 12:08:51 -0700

    - Created a builder that mixes the fluent interface with the facade pattern. If our builder is more complex and within our main builder we need another build to build up a different aspect then we want to fluently jump between different types of builders but continue working on the same common model we are trying to build.

---
+++ src/com/nfragiskatos/CreationalPatterns/Builder/FacetedBuilder/PersonBuilder.java
@@ -0,0 +1,17 @@
+package com.nfragiskatos.CreationalPatterns.Builder.FacetedBuilder;
+
+public class PersonBuilder {
+    protected Person person = new Person();
+
+    public PersonAddressBuilder lives() {
+        return new PersonAddressBuilder(person);
+    }
+
+    public PersonJobBuilder works() {
+        return new PersonJobBuilder(person);
+    }
+
+    public Person build() {
+        return person;
+    }
+}
