a0b1f9d57ee7c515cea3ee4f110cf4c42d491f12
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-21 20:11:17 -0700

    - Created HtmlBuilder for HtmlElements - Implemented the toString for HtmlElement that knows how to build it's formatted html string.

--- src/com/nfragiskatos/CreationalPatterns/Builder/HtmlElement.java
+++ src/com/nfragiskatos/CreationalPatterns/Builder/HtmlElement.java
@@ -1,6 +1,7 @@
 package com.nfragiskatos.CreationalPatterns.Builder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class HtmlElement {
@@ -16,5 +17,35 @@ public class HtmlElement {
     public HtmlElement(String name, String text) {
         this.name = name;
         this.text = text;
+    }
+
+    private String toStringImpl(int indent) {
+
+        // Create tag with proper indent
+        StringBuilder sb = new StringBuilder();
+        String i = String.join("", Collections.nCopies(indent * indentSize, " "));
+        sb.append(String.format("%s<%s>%s", i, name, newLine));
+
+        // append internal text (if any)
+        if (text != null && !text.isEmpty()) {
+            sb.append(String.join("", Collections.nCopies(indentSize*(indent + 1), " ")))
+                    .append(text)
+                    .append(newLine);
+        }
+
+        // Recursive part to also print all internal elements
+        for (HtmlElement e : elements) {
+            sb.append(e.toStringImpl(indent + 1));
+        }
+
+        // print closing tag
+        sb.append(String.format("%s</%s>%s", i, name, newLine));
+
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toStringImpl(0);
     }
 }
