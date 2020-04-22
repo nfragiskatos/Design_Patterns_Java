ca8a5b1bd9216a252bd6ba6279c9a6b1dd0d6e6b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-21 20:15:04 -0700

    - Gave the HtmlBuilder a fluent interface. (addChild just returns itself instead of void)

--- src/com/nfragiskatos/CreationalPatterns/Builder/BuilderMain.java
+++ src/com/nfragiskatos/CreationalPatterns/Builder/BuilderMain.java
@@ -3,9 +3,9 @@ package com.nfragiskatos.CreationalPatterns.Builder;
 public class BuilderMain {
 
     public static void main(String[] args) {
-        HtmlBuilder builder = new HtmlBuilder("ul");
-        builder.addChild("li", "hello");
-        builder.addChild("li", "world");
+        HtmlBuilder builder = new HtmlBuilder("ul")
+                .addChild("li", "hello")
+                .addChild("li", "world");
         System.out.println(builder);
     }
 }
