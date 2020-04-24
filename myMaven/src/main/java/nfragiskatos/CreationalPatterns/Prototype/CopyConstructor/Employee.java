f04851cd89a620e28cd77b2490d3c989a9753a1b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-24 13:09:14 -0700

    - Creating deep copies using copy constructor pattern. More palatable alternative to implementing Cloneable.

---
+++ myMaven/src/main/java/nfragiskatos/CreationalPatterns/Prototype/CopyConstructor/Employee.java
@@ -0,0 +1,24 @@
+package nfragiskatos.CreationalPatterns.Prototype.CopyConstructor;
+
+public class Employee {
+
+    public String name;
+    public Address address;
+
+    public Employee(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Employee(Employee other) {
+        this(other.name, new Address(other.address));
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
