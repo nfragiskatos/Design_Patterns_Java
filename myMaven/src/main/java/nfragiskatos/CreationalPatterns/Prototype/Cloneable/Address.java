feb409d5910a0f225f2f582add1da5118fff5a1d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-24 13:07:58 -0700

    - Creating deep copies with the Cloneable interface. Cloneable is not recommended because there are no guarantees on it what type of copy you are going to get (deep vs shallow)

---
+++ myMaven/src/main/java/nfragiskatos/CreationalPatterns/Prototype/Cloneable/Address.java
@@ -0,0 +1,25 @@
+package nfragiskatos.CreationalPatterns.Prototype.Cloneable;
+
+public class Address implements Cloneable {
+
+    public String streetName;
+    public int houseNumber;
+
+    public Address(String streetName, int houseNumber) {
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetName='" + streetName + '\'' +
+                ", houseNumber=" + houseNumber +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Address(streetName, houseNumber);
+    }
+}
