759772ea7fc72c1b38ca2b70e52bd0c8655e1dc1
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-04-25 09:27:18 -0700

    - Because serialization breaks the Singleton patter, we can add a readResolve method to our object that will be used when the object is being read from the stream, so we can control what gets passed back.

--- myMaven/src/main/java/nfragiskatos/CreationalPatterns/Singleton/BasicSingleton/BasicSingleton.java
+++ myMaven/src/main/java/nfragiskatos/CreationalPatterns/Singleton/BasicSingleton/BasicSingleton.java
@@ -1,8 +1,11 @@
 package nfragiskatos.CreationalPatterns.Singleton.BasicSingleton;
 
-public class BasicSingleton {
+import java.io.Serializable;
+
+public class BasicSingleton implements Serializable {
 
     private int value = 0;
+    private static final BasicSingleton INSTANCE = new BasicSingleton();
 
     public int getValue() {
         return value;
@@ -16,7 +19,14 @@ public class BasicSingleton {
         
     }
 
-    private static final BasicSingleton INSTANCE = new BasicSingleton();
+    /*
+    Just by implementing this method, it's a way to fix the issue with serialization breaking the singleton pattern.
+
+    Allows a class to replace/resolve the object read from the stream before it is returned to the caller.
+     */
+    protected Object readResolve() {
+        return INSTANCE;
+    }
 
     public static BasicSingleton getInstance() {
         return INSTANCE;
--- myMaven/src/main/java/nfragiskatos/CreationalPatterns/Singleton/BasicSingleton/BasicSingletonMain.java
+++ myMaven/src/main/java/nfragiskatos/CreationalPatterns/Singleton/BasicSingleton/BasicSingletonMain.java
@@ -1,12 +1,57 @@
 package nfragiskatos.CreationalPatterns.Singleton.BasicSingleton;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class BasicSingletonMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        // Basic use case of singleton
+//        BasicSingleton singleton = BasicSingleton.getInstance();
+//
+//        singleton.setValue(123);
+//        System.out.println(singleton.getValue());
+
+        // Problems:
+
+        /*
+            1. Reflection: This is the very out of your way approach to really purposely break the singleton paradigm
+
+            2. Serialization: Less obvious, e.g. just reading from a file. As shown below.
+         */
 
         BasicSingleton singleton = BasicSingleton.getInstance();
+        singleton.setValue(111);
 
-        singleton.setValue(123);
+        String filename = "singleton.bin";
+
+        saveToFile(singleton, filename);
+
+        singleton.setValue(222);
+
+        BasicSingleton singleton2 = readFromFile(filename);
+
+        System.out.println(singleton == singleton2);
         System.out.println(singleton.getValue());
+        System.out.println(singleton2.getValue());
+    }
+
+    static void saveToFile(BasicSingleton singleton, String filename) throws Exception {
+
+        try (FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(singleton);
+        }
+    }
+
+    static BasicSingleton readFromFile(String filename) throws Exception {
+        try (FileInputStream fileIn = new FileInputStream(filename);
+             ObjectInputStream in = new ObjectInputStream(fileIn)
+        ) {
+            return (BasicSingleton) in.readObject();
+        }
     }
 }
