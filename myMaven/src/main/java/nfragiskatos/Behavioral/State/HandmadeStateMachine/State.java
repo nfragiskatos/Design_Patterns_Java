1b4ac77af16527e60422be20019ea736fc341c01
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-14 17:13:14 -0700

    - Implementation of our own state machine. Essentially just boils down to a Map that specifies for any given State there are a list of possible transitions and their associated resulting states.

---
+++ myMaven/src/main/java/nfragiskatos/Behavioral/State/HandmadeStateMachine/State.java
@@ -0,0 +1,11 @@
+package nfragiskatos.Behavioral.State.HandmadeStateMachine;
+
+public enum State {
+    OFF_HOOK,   // starting
+    ON_HOOK,    // terminal
+    CONNECTING,
+    CONNECTED,
+    ON_HOLD
+}
+
+
