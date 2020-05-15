ca965b70a30b92e6049414ad9e6aab5fa45f2420
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-15 16:41:58 -0700

    - Using built in state machine from Spring Framework.

---
+++ myMaven/src/main/java/nfragiskatos/Behavioral/State/SpringMachine/Events.java
@@ -0,0 +1,11 @@
+package nfragiskatos.Behavioral.State.SpringMachine;
+
+public enum Events {
+    CALL_DIALED,
+    HUNG_UP,
+    CALL_CONNECTED,
+    PLACED_ON_HOLD,
+    TAKEN_OFF_HOLD,
+    LEFT_MESSAGE,
+    STOP_USING_PHONE
+}
---
+++ myMaven/src/main/java/nfragiskatos/Behavioral/State/SpringMachine/States.java
@@ -0,0 +1,11 @@
+package nfragiskatos.Behavioral.State.SpringMachine;
+
+public enum States {
+    OFF_HOOK,   // starting
+    ON_HOOK,    // terminal
+    CONNECTING,
+    CONNECTED,
+    ON_HOLD
+}
+
+
